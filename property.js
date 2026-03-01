2d2b5089d87f1b0b87ec377810233a99fd25d364
Rhishikesh Prince <rhishikeshprince32@gmail.com>
2026-03-01 08:19:27 +0530

    ready for hosting

--- property.js
+++ property.js
@@ -6,3 +6,16 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navLinks.classList.toggle("active");
 });
+
+
+// THHUMB 
+ function changeHero(element) {
+        const mainImage = document.getElementById("mainHeroImage");
+
+        mainImage.style.opacity = 0;
+
+        setTimeout(() => {
+            mainImage.src = element.src;
+            mainImage.style.opacity = 1;
+        }, 200);
+    }
